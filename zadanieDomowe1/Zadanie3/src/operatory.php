dfa9f88e8e23a2bea16ea51b955b9ba142136c45
Szymon Nowakowski <79590251+nowakowskiszymon@users.noreply.github.com>
2021-11-04 15:15:28 +0100

    Add files via upload

---
+++ zadanieDomowe1/Zadanie3/src/operatory.php
@@ -0,0 +1,12 @@
+<?php
+$operatory = "OPERATORY";
+$liczba = 2;
+$slowo = "słowo";
+$liczba2 = 2.333;
+echo $operatory . '</br>';
+echo $liczba . '</br>'; 
+echo $slowo . '</br>';
+echo $liczba2 . '</br>';
+echo $liczba * $liczba2 . '</br>';
+echo $liczba / 0;
+?>
---
+++ zadanieDomowe1/Zadanie5/index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+      include ('src/petle.php') 
+    ?> 
+  </body>
+</html>
