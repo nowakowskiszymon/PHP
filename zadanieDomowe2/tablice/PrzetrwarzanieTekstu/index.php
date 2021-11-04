e3852d6b46bb105449d6e553caf98601ebd87048
Szymon Nowakowski <79590251+nowakowskiszymon@users.noreply.github.com>
2021-11-04 19:36:10 +0100

    Delete zadanieDomowe2/tablice directory

--- zadanieDomowe2/tablice/PrzetrwarzanieTekstu/index.php
+++
@@ -1,8 +0,0 @@
-<div class="flex flex-row flex-wrap w-full justify-center font-bold text-3xl mb-2">
-  Tekst
-</div>
-<div class="flex flex-row flex-wrap w-full justify-center text-xl mb-2">
-  <?php
-    include ('src/tekst.php');
-  ?>
-</div>
